5547cec376da41caae6515e469cde249980aca41
BurnsD <Dustinburnsactech@gmail.com>
2022-04-23 01:20:07 -0500

    update cart test passed

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -37,7 +37,21 @@ export const reducer = (state, action) => {
                 cartOpen: true,
                 cart: [...state.cart, action.product]
             };
-            
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
+            };
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            };
 
         default:
             return state;
