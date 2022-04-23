9147ba89ccd18665d65c589f9d38ec41d94e475c
BurnsD <Dustinburnsactech@gmail.com>
2022-04-22 22:20:00 -0500

    all reducers test pass

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -27,7 +27,7 @@ import {
   });
 
   // test categories
-  
+
   test('UPDATE_CATEGORIES', () => {
     let newState = reducer(initialState, {
       type: UPDATE_CATEGORIES,
@@ -36,4 +36,14 @@ import {
   
     expect(newState.categories.length).toBe(2);
     expect(initialState.categories.length).toBe(1);
+  });
+
+  test('UPDATE_CURRENT_CATEGORY', () => {
+    let newState = reducer(initialState, {
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: '2'
+    });
+  
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
   });
