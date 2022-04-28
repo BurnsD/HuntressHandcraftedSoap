ae99bf86783519966fdf44ba0c95f92371776b17
Hunter Nelson <huntknelson@gmail.com>
2022-04-28 15:10:25 -0500

    cleaned up signupform

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -8,7 +8,6 @@ function About() {
             <div className='pt-5 text-dark'>
                 <h1 className='script text-light'>Strive for Something Better</h1>
             </div>
-            {/* <div className='flex pb-5' id='aboutBar'> */}
             <div className='pb-5'>
                 <p className='text-dark'>
                     Our soap is the best soap. There is no better soap. Any other soap wishes it was as good as our soap, hell, anything wishes it was as high quality and sustainably sourced as our soap. I mean, who wouldn't want soap infused with the finest Palm Oil supplied by none other than the Roundtable on Sustainable Palm Oil? You can't forget about the Tallow in these bad boys either. We exclusively use GMO Free, Grass-fed, Certified-human-raised-and-handled tallow to ensure our customers enjoy ethically produced, foamy, creamy bubbles. It feels good to be great.
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,16 +2,21 @@ import React, { useState } from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
+import About from "../components/About";
+import Hero from "../components/Hero";
+import { Container } from "react-bootstrap"
 
 const Home = () => {
   const [currentCategory, setCategory] = useState("");
 
   return (
-    <div className="container">
+    <Container fluid>
+      <Hero />
+      <About />
       <CategoryMenu setCategory={setCategory} />
       <ProductList currentCategory={currentCategory} />
       <Cart />
-    </div>
+    </Container>
   );
 };
 
