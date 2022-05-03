3e58f1109dffe4e0ad8344e70fe72704a69e148d
unknown <steinway.nathan@gmail.com>
2022-05-03 15:35:04 -0500

    moved dotenv to top of serverjs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
@@ -9,7 +10,6 @@ const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-require('dotenv').config();
 
 
 const server = new ApolloServer({
